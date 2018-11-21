475aafa99df67eaaebe3195de660689f78292cad
Hoda0123 <43361728+Hoda0123@users.noreply.github.com>
2018-11-22 01:22:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,7 +117,27 @@ client.on("message", message => {
 };     
 });
 
-
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'cmd');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("**SAVE **")
+        .setTimestamp()
+    
+      channel.sendEmbed(embed);
+    });
 
 
 client.on('ready', () => {
