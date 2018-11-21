5aeca8885833aa8877e98b2f5f9777e3562a6099
Hoda0123 <43361728+Hoda0123@users.noreply.github.com>
2018-11-22 01:14:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,7 +100,17 @@ client.on('message', msg => {
   }
 });
  
-
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel = member.guild.channels.find('name', 'cmd')
+if (!channel) return;
+channel.send({embed : embed});
+});
 
 var prefix = "h";
 client.on("message", message => {
