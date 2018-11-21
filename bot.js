de32fd63ee57fadb41407f7bfc3865e8a3576438
Hoda0123 <43361728+Hoda0123@users.noreply.github.com>
2018-11-22 01:06:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -151,7 +151,27 @@ client.on("message", function(message) {
     message.reply("**لا تسب يا توتو**");
   };
 });
+
+client.on("message", message => {
+              var args = message.content.substring(prefix.length).split("h");
+              if (message.content.startsWith(prefix + "مسح")) {
+                  if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
+     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**⚠  لا يوجد لديك صلاحية لمسح الشات**');
+          var msg;
+          msg = parseInt();
+        
+        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+        message.channel.sendMessage("", {embed: {
+          title: "``تــم مسح الشات ``",
+          color: 0x06DF00,
+          footer: {
+            
+          }
+        }}).then(msg => {msg.delete(3000)});
+                            }
   
+       
+  });
 
 
  client.on('message', message => {
