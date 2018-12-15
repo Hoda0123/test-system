8eb193e5cdc4e3500af59eab50ddb5cb0a19a8aa
Hoda0123 <43361728+Hoda0123@users.noreply.github.com>
2018-12-15 12:43:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -267,6 +267,63 @@ guild.owner.send(`سيرفر : ${guild.name}
 }
 });
 
+
+client.on('message', async message =>{
+    var prefix = "%";
+  if (message.author.omar) return;
+  if (!message.content.startsWith(prefix)) return;
+  if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('MANAGE_ROLES'));
+  if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return;
+  var command = message.content.split(" ")[0];
+    let mention = message.mentions.members.first();
+  command = command.slice(prefix.length);
+  var args = message.content.split(" ").slice(1);
+      if(command == "mute") {
+      let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      if(!tomute) return message.reply("**يجب عليك المنشن اولاّ**:x: ") .then(m => m.delete(5000));
+      if(mention.id === message.author.id) return message.reply('**لا يمكنك اعطاء ميوت  لنفسك**').then(m => m.delete(5000));
+   
+      if(mention.highestRole.position >= message.guild.member(message.author).highestRole.positon) return message.reply('**لا يمكنك اعطاء لميوت شخص رتبته اعلى منك**').then(m => m.delete(5000));
+      if(tomute.hasPermission("MANAGE_MESSAGES"))return;
+      let muterole = message.guild.roles.find(`name`, "Muted");
+      //start of create role
+      if(!muterole){
+        try{
+          muterole = await message.guild.createRole({
+            name: "Muted",
+            color: "#000000",
+            permissions:[]
+          })
+          message.guild.channels.forEach(async (channel, id) => {
+            await channel.overwritePermissions(muterole, {
+              SEND_MESSAGES: false,
+              ADD_REACTIONS: false
+            });
+          });
+        }catch(e){
+          console.log(e.stack);
+        }
+      }
+      //end of create role
+      let mutetime = args[1];
+      if(!mutetime) return message.reply("**يرجى تحديد وقت الميوت**:x:");
+    
+      await(tomute.addRole(muterole.id));
+      message.reply(`<@${tomute.id}> تم اعطائه ميوت ومدة الميوت : ${ms(ms(mutetime))}`);
+    
+      setTimeout(function(){
+        tomute.removeRole(muterole.id);
+        message.channel.send(`<@${tomute.id}> **انقضى الوقت وتم فك الميوت عن الشخص**:white_check_mark: `);
+      }, ms(mutetime));
+    
+    
+    //end of module
+    }
+  
+  });
+
+
 client.on('message' , message => {
     var prefix = "%";
     let user = message.mentions.users.first()|| client.users.get(message.content.split(' ')[1])
